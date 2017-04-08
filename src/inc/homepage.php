933aa3367410b1878c42023be52df5e7cf2f1a6b
roximonoxide <weismachen@gmail.com>
2017-04-07 22:01:34 -0400

    Update homepage.php

--- src/inc/homepage.php
+++ src/inc/homepage.php
@@ -3,9 +3,9 @@
 
       <!-- Jumbotron -->
       <div class="jumbotron">
-        <h1>Expore your partner!</h1>
+        <h1>Share kinks with your partner(s)</h1>
         <p class="lead">
-         "We Should Try It" is an online sex questionnaire for couples. Once you and your partner answer all the questions about various sexual fantasies, we display only sex fantasies <strong>you both into</strong>.
+         Cum Closer is a sex questionnaire that aims to bring you and your partner(s) closer together by discovering mutual intersts. Answer the questions with your partner(s) an discover each other's sexual intersts. Choose to display only sex fantasies <strong>you're into</strong> or filter answers by degree of interest.
          
         
         </p>
@@ -28,13 +28,13 @@
       <div class="row">
         <div class="col-lg-4">
           <h2>Fully anonymous.</h2>          
-          <p>You don't have to supply any information about yourself or your partner. With fully encrypted (SSL) connection your privacy is guaranteed.
+          <p>We don't ask for any information about yourself or your partner(s). With fully encrypted (SSL) connection your privacy is guaranteed.
           
           </p>          
         </div>
         <div class="col-lg-4">
           <h2>No registration required.</h2>
-          <p>No registration is required, simply click "Get started" button to begin. Your partner can use same or any other computer.
+          <p>Click "Get started" when you're to begin. Your partner can use same or any other computer.
           
            </p>
        </div>
@@ -42,7 +42,7 @@
           <h2>Customization available.</h2>
           <p>
           Customize the questions and categories by adding your own. 
-          Answer only questions you want. Your own questions will not stand out from the common list.  
+          Answer only questions you want. Questions you add do not stand out from the basic questionnaire list.
           </p>          
         </div>
       </div>
